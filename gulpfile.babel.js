a6cbb544ee5ed91a81ce430a801d7f09ce911bc0
comr4d <comr4d@yandex.ru>
2021-09-22 16:59:07 +0300

    test

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -2,9 +2,7 @@ import config from './gulp/config';
 
 config.setEnv();
 
-// exports.test = () => {
-//   console.log(process.argv.includes('--prod'));
-// };
+
 
 export const build = () => {
   console.log(config.isProd);
